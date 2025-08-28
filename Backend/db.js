9bce803777ae3ab5c573bd10bb82141dc71cc5c0
Madusha Lakshan <dmmadushalw1999@gmail.com>
2025-08-28 20:33:23 +0530

    update mongo cluster

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://dmmadushalw1999:2subr0nbKyxHCZdU@firstcluster.uytooc1.mongodb.net/?retryWrites=true&w=majority&appName=FirstCluster";
+const mongoURI = "{mongo cluster URL}";
 
 const connectToMongo = async () => {
   try {
